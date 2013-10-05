03aa603e2528f39fe5381b9f38ac76fac20c1aeb
Jared Wolff <jaredwolff@me.com>
2013-10-05 01:40:45 -0700

    Adding the ability to cache GA data locally. Now there is a refresh_rate at which you can set how fresh you want the GA data to be. Set to 0 to disable this function.

--- jekyll-ga.rb
+++ jekyll-ga.rb
@@ -18,45 +18,72 @@ module Jekyll
       end
 
       ga = site.config['jekyll_ga']
-      client = Google::APIClient.new(
-        :application_name => ga['application_name'],
-        :application_version => ga['application_version'])
+      # Local cache setup so we don't hit the sever X amount of times for same data.
+      cache_directory = ga['cache_directory'] || "_jekyll_ga"
+      cache_filename = ga['cache_filename'] || "ga_cache.json"
+      cache_file_path = cache_directory + "/" + cache_filename
+      response_data = nil
 
-      # Load our credentials for the service account
-      key = Google::APIClient::KeyUtils.load_from_pkcs12(ga['key_file'], ga['key_secret'])
-      client.authorization = Signet::OAuth2::Client.new(
-        :token_credential_uri => 'https://accounts.google.com/o/oauth2/token',
-        :audience => 'https://accounts.google.com/o/oauth2/token',
-        :scope => 'https://www.googleapis.com/auth/analytics.readonly',
-        :issuer => ga['service_account_email'],
-        :signing_key => key)
+      # Set the refresh rate in minutes (how long the program will wait before writing a new file)
+      refresh_rate = ga['refresh_rate'] || 60
 
-      # Request a token for our service account
-      client.authorization.fetch_access_token!
-      analytics = client.discovered_api('analytics','v3')
-
-      params = {
-        'ids' => ga['profileID'],
-        'start-date' => Chronic.parse(ga['start']).strftime("%Y-%m-%d"),
-        'end-date' => Chronic.parse(ga['end']).strftime("%Y-%m-%d"),
-        'dimensions' => "ga:pagePath",
-        'metrics' => ga['metric'],
-        'max-results' => 10000
-      }
-      if ga['segment']
-        params['segment'] = ga['segment']
-      end
-      if ga['filters']
-        params['filters'] = ga['filters']
+      # If the directory doesn't exist lets make it
+      if not Dir.exist?(cache_directory)
+        Dir.mkdir(cache_directory)
       end
 
-      response = client.execute(:api_method => analytics.data.ga.get, :parameters => params)
+      # Now lets check for the cache file and how old it is
+      if File.exist?(cache_file_path) and ((Time.now - File.mtime(cache_file_path))/60 < refresh_rate)
+        response_data = JSON.parse(File.read(cache_file_path));
+      else
 
-      if response.error?
-        abort("Client Execute Error: #{response.error_message}")
+        client = Google::APIClient.new(
+          :application_name => ga['application_name'],
+          :application_version => ga['application_version'])
+
+        # Load our credentials for the service account
+        key = Google::APIClient::KeyUtils.load_from_pkcs12(ga['key_file'], ga['key_secret'])
+        client.authorization = Signet::OAuth2::Client.new(
+          :token_credential_uri => 'https://accounts.google.com/o/oauth2/token',
+          :audience => 'https://accounts.google.com/o/oauth2/token',
+          :scope => 'https://www.googleapis.com/auth/analytics.readonly',
+          :issuer => ga['service_account_email'],
+          :signing_key => key)
+
+        # Request a token for our service account
+        client.authorization.fetch_access_token!
+        analytics = client.discovered_api('analytics','v3')
+
+        params = {
+          'ids' => ga['profileID'],
+          'start-date' => Chronic.parse(ga['start']).strftime("%Y-%m-%d"),
+          'end-date' => Chronic.parse(ga['end']).strftime("%Y-%m-%d"),
+          'dimensions' => "ga:pagePath",
+          'metrics' => ga['metric'],
+          'max-results' => 10000
+        }
+        if ga['segment']
+          params['segment'] = ga['segment']
+        end
+        if ga['filters']
+          params['filters'] = ga['filters']
+        end
+
+        response = client.execute(:api_method => analytics.data.ga.get, :parameters => params)
+
+        if response.error?
+          abort("Client Execute Error: #{response.error_message}")
+        end
+
+        response_data = response.data
+
+        File.open(cache_file_path,"w") do |f|
+          f.write(response_data.to_json)
+        end
+
       end
 
-      results = Hash[response.data.rows]
+      results = Hash[response_data["rows"]]
 
       site.posts.each { |post|
         url = post.url + '/'
